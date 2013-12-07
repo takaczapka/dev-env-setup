f73256409cf5e08c1b54da1d1e37fae05b0e80f6
takaczapka <takaczapka@gmail.com>
2013-12-07 18:16:23 +0000

    removed aliases not supported by osx (--color), added dev shortcut

--- setuplocalenv.sh
+++ setuplocalenv.sh
@@ -1,20 +1,12 @@
 export PATH=$HOME/bin:$PATH
 
 # Aliases
-alias ls='ls --color=auto'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
-
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
 alias d='cd $HOME/Downloads'
-
+alias dev='cd $HOME/dev'
 # SBT 
 export SBT_OPTS="-Xmx1024M -XX:MaxPermSize=512m -XX:ReservedCodeCacheSize=128m"
 
