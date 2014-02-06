c1216b976766f82b7ea7d2a9d6ae05c55c4c4378
takaczapka <takaczapka@gmail.com>
2014-02-06 11:07:34 +0000

    Update setuplocalenv.sh

--- setuplocalenv.sh
+++ setuplocalenv.sh
@@ -10,17 +10,9 @@ alias e='exit'
 alias d='cd $HOME/Downloads'
 alias dev='cd $HOME/dev'
 alias u='cd ..'
+
 # SBT 
 export SBT_OPTS="-Xmx1024M -XX:MaxPermSize=512m -XX:ReservedCodeCacheSize=128m"
-
-# GIT
-alias gst='git status'
-alias gadd='git add .'
-gitAddCommit() { git add .; git commit -m "$1";}
-alias gcommit='git commit -m "$1"'
-alias gac=gitAddCommit
-alias gpull='git pull --rebase'
-alias gpush='gpull; git push'
 
 
 
