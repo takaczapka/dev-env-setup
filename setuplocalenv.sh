3198b7dbfd5ef016f732148f34faa6240b109958
Maciej Falski <maciej.falski@db.com>
2014-03-10 11:41:28 +0000

    added jkill command

--- setuplocalenv.sh
+++ setuplocalenv.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export PATH=$HOME/bin:$PATH
 
 # Aliases
@@ -14,5 +16,14 @@ alias u='cd ..'
 # SBT 
 export SBT_OPTS="-Xmx1024M -XX:MaxPermSize=512m -XX:ReservedCodeCacheSize=128m"
 
-
-
+function jkill(){
+  local to_kill=$1
+  local pids=`jps | grep -i $to_kill | awk '{ print $1 }'`
+  if [[ -n "$pids" ]]
+  then
+    kill -9 $pids
+    echo "Killed Java processes for" \"$to_kill\" "with pid(s):" $pids
+  else 
+    echo "Nothing to kill"
+  fi
+}
