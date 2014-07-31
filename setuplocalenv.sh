de775b1ceb4cbe71171a46c8b62248745045a851
Maciej Falski <maciej.falski@db.com>
2014-07-31 14:03:58 +0100

    a few bash functions

--- setuplocalenv.sh
+++ setuplocalenv.sh
@@ -12,6 +12,16 @@ alias e='exit'
 alias d='cd $HOME/Downloads'
 alias dev='cd $HOME/dev'
 alias u='cd ..'
+alias pp="ps axuf | pager"
+
+# PSEUDO ALIASES FOR COMMONLY USED LONG COMMANDS 
+# from http://cfenollosa.com/misc/tricks.txt
+function lt() { ls -ltrsa "$@" | tail; }
+function psgrep() { ps axuf | grep -v grep | grep "$@" -i --color=auto; }
+function fname() { find . -iname "*$@*"; }
+#  removes lines from $1 if they appear in $2
+function remove_lines_from() { grep -F -x -v -f $2 $1; }
+function mcd() { mkdir $1 && cd $1; }
 
 # SBT 
 export SBT_OPTS="-Xmx1024M -XX:MaxPermSize=512m -XX:ReservedCodeCacheSize=128m"
@@ -27,3 +37,4 @@ function jkill(){
     echo "Nothing to kill"
   fi
 }
+
