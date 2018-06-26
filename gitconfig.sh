ecb26a15a32b27874bc7a9a3ced8183addd69d6e
Maciej Falski <maciej@previ.se>
2018-06-26 11:18:14 +0100

    improved log output formatting

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,17 +7,16 @@ git config --global alias.nuke '!git checkout -f && git clean -f -d'
 git config --global alias.add-commit-push '!git add -A && git commit && git push'
 git config --global color.ui true
 # formatting of git log ouput
-git config --global format.pretty oneline --graph
+git config --global format.pretty %Cgreen%ar\ %Cred%h\ %Cblue%aN\ \<%ae\>:\ %Creset%s --graph 
 # make all git pull commands will integrate via git rebase instead of git merge.
 git config --global branch.autosetuprebase always
 # deny using commands such as push --force  (overwrites the structure and sequence of commits on the authoritative repository, throwing away other people's commits)
 git config --global receive.denyNonFastForwards true
-# list all alises 
+# list all alises
 git config --global alias.aliases '!git config --get-regexp alias'
 # always rebase
 git config --global alias.pu '!git pull --rebase'
 # colorful log
-git config --global alias.l '!git log --graph --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)" --abbrev-commit --date=relative'
+git config --global alias.l '!git log --graph --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)ï¿½ %an%C(reset)%C(bold yellow)%d%C(reset)" --abbrev-commit --date=relative'
 git config --global alias.yesterday '!git l --graph --all --since=day.before.yesterday.midnight --until=midnight'
 git config --global alias.today '!git l --graph --all --since=midnight'
-
