a156b4950ced9d050c4a9f5ae9074837ec4894d8
Maciej Falski <maciej@previ.se>
2019-02-07 10:41:30 +0000

    Default lein user profile

---
+++ clojure/.lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[venantius/ultra "0.5.2"]
+                  [com.jakemccrary/lein-test-refresh "0.23.0"]
+                  [jonase/eastwood "0.3.5"]
+                  [lein-pprint "1.1.1"]]}}
