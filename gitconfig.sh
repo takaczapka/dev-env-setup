1be8a8245459f5459751e1ee0cec222410a2c3a9
Maciej Falski <maciej@previ.se>
2019-09-19 14:03:02 +0100

    Fix git log formatting

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,7 +7,7 @@ git config --global alias.nuke '!git checkout -f && git clean -f -d'
 git config --global alias.add-commit-push '!git add -A && git commit && git push'
 git config --global color.ui true
 # formatting of git log ouput
-git config --global format.pretty %Cgreen%ar\ %Cred%h\ %Cblue%aN\ \<%ae\>:\ %Creset%s --graph 
+git config --global format.pretty '%Cgreen%ar %Cred%h %Cblue%aN <%ae>: %Creset%s'
 # make all git pull commands will integrate via git rebase instead of git merge.
 git config --global branch.autosetuprebase always
 # deny using commands such as push --force  (overwrites the structure and sequence of commits on the authoritative repository, throwing away other people's commits)
